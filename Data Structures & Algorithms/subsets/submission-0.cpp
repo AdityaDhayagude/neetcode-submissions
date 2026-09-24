4c2176f67c3d93a09fbf816299b98c6e30815ac0
Aditya Dhayagude <110342386+AdityaDhayagude@users.noreply.github.com>
2026-09-24 11:32:55 +0100

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void getSubsets(int idx, vector<int> &nums, vector<vector<int>> &ans, vector<int> &ds){
+        ans.push_back(ds);
+
+        for(int i = idx; i<nums.size(); i++){
+            if(i> idx && nums[i] == nums[i-1]) continue;
+
+            ds.push_back(nums[i]);
+            getSubsets(i+1, nums, ans, ds);
+            ds.pop_back();
+        }
+
+        return;
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds{};
+
+        getSubsets(0, nums, ans, ds);
+
+        return ans;
+    }
+};
